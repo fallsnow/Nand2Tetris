bae5673c32e2f9b7011e7252e9f35d249ec79482
unknown <10001164414@z0003005322.dnjp.globaldenso>
2016-08-05 20:01:10 +0900

    in progress

--- projects/11/CompilationEngine.rb
+++ projects/11/CompilationEngine.rb
@@ -10,21 +10,27 @@ class CompilationEngine
         @type = nil
         @kind = nil
         @purpose = :define
+        @function_name = ""
+        @local_num = 0
+        @expression_num = 0
 
-        @jackfile = jackfile
         xmlfile = jackfile.sub(/\.jack$/, ".xml")
         @fo = File.open(xmlfile, "w")
+
+        vmfile = jackfile.sub(/\.jack$/, ".vm")
+        @class_name = File.basename(jackfile, ".jack")
+        #@fo = File.open(vmfile, "w")
         
         @tokenizer = JackTokenizer.new(jackfile)
         @symbol_table = SymbolTable.new
-        @vmwriter = VMWriter.new
+        @vmwriter = VMWriter.new(vmfile)
     end
     
     # 'class' className '{' classVarDec* subroutineDec* '}'
     def compile_class
         @tokenizer.has_more_tokens? ? @tokenizer.advance : exit - 1
         
-        @fo.puts "<class>"
+        #@fo.puts "<class>"
         if accept?(TokenType::KEYWORD, ["class"])
             expect(TokenType::IDENTIFIER)
             expect(TokenType::SYMBOL, ["{"])
@@ -42,7 +48,8 @@ class CompilationEngine
             if @tokenizer.has_more_tokens?
                 STDERR.print "Parse Error: Unused tokens are left"
             else
-                @fo.puts "</class>"
+                #@fo.puts "</class>"
+                @vmwriter.close
             end
         end
     end
@@ -50,18 +57,21 @@ class CompilationEngine
     private
     
     def compile_class_var_dec
-        @fo.puts "<classVarDec>"
+        #@fo.puts "<classVarDec>"
+        @local_num = 0
         if accept?(TokenType::KEYWORD, ["static", "field"])
             @kind = @tokenizer.keyword
             expect_type
             expect(TokenType::IDENTIFIER)
+            @local_num += 1
             while accept?(TokenType::SYMBOL, [","]) do
                 expect(TokenType::IDENTIFIER)
+                @local_num += 1
             end
             
             expect(TokenType::SYMBOL, [";"])
         end
-        @fo.puts "</classVarDec>"   
+        #@fo.puts "</classVarDec>"   
     end
     
     # ('constructor'|'function'|'method') ('void'|type) subroutinName
@@ -70,21 +80,23 @@ class CompilationEngine
         @symbol_table.start_subroutine
         @purpose = :define
 
-        @fo.puts "<subroutineDec>"
+        #@fo.puts "<subroutineDec>"
         if accept?(TokenType::KEYWORD, ["constructor", "function", "method"])
             @kind = "subroutine"
             accept?(TokenType::KEYWORD, ["void"]) or accept_type?
             expect(TokenType::IDENTIFIER)
+            @function_name = "#{@class_name}.#{@tokenizer.identifier}"
             expect(TokenType::SYMBOL, ["("])
             compile_parameter_list
             expect(TokenType::SYMBOL, [")"])
         end
         
-        @fo.puts "<subroutineBody>"
+        #@fo.puts "<subroutineBody>"
         if accept?(TokenType::SYMBOL, ["{"])
             while apply?(TokenType::KEYWORD, ["var"]) do 
                 compile_var_dec
             end
+            @vmwriter.write_function(@function_name, @local_num)
             @purpose = :use
             compile_statements
             expect(TokenType::SYMBOL, ["}"])
@@ -94,7 +106,7 @@ class CompilationEngine
     end
     
     def compile_parameter_list
-        @fo.puts "<parameterList>"
+        #@fo.puts "<parameterList>"
         if accept_type?
             @kind = "argument"
             expect(TokenType::IDENTIFIER)
@@ -104,11 +116,11 @@ class CompilationEngine
                 expect(TokenType::IDENTIFIER)
             end
         end
-        @fo.puts "</parameterList>"
+        #@fo.puts "</parameterList>"
     end
     
     def compile_var_dec
-        @fo.puts "<varDec>"
+        #@fo.puts "<varDec>"
         if accept?(TokenType::KEYWORD, ["var"])
             @kind = @tokenizer.keyword
             expect_type
@@ -120,34 +132,40 @@ class CompilationEngine
             end
             expect(TokenType::SYMBOL, [";"])
         end
-        @fo.puts "</varDec>"   
+        #@fo.puts "</varDec>"   
     end
     
     def compile_statements
-        @fo.puts "<statements>"
+        #@fo.puts "<statements>"
         while apply?(TokenType::KEYWORD, ["let", "if", "while", "do", "return"]) do
             send("compile_#{@tokenizer.keyword}")
         end
-        @fo.puts "</statements>"
+        #@fo.puts "</statements>"
     end
     
     def compile_do
-        @fo.puts "<doStatement>"    
+        #@fo.puts "<doStatement>"
+        function_name = ""    
         if accept?(TokenType::KEYWORD, ["do"])
             expect(TokenType::IDENTIFIER)
+            @function_name = @tokenizer.identifier
             if accept?(TokenType::SYMBOL, ["."])
                 expect(TokenType::IDENTIFIER)
+                @function_name += ".#{@tokenizer.identifier}"
             end
             expect(TokenType::SYMBOL, ["("])
             compile_expression_list
             expect(TokenType::SYMBOL, [")"])
             expect(TokenType::SYMBOL, [";"])
-        end    
-        @fo.puts "</doStatement>"
+        end
+        # put arguments to the stack
+        #@fo.puts "call #{function_name} #{@expression_num}"
+        @vmwriter.write_call(@function_name, @expression_num)    
+        #@fo.puts "</doStatement>"
     end
     
     def compile_let
-        @fo.puts "<letStatement>"
+        #@fo.puts "<letStatement>"
         if accept?(TokenType::KEYWORD, ["let"])
             expect(TokenType::IDENTIFIER)
             if accept?(TokenType::SYMBOL, ["["])
@@ -158,7 +176,7 @@ class CompilationEngine
             compile_expression
             expect(TokenType::SYMBOL, [";"])
         end
-        @fo.puts "</letStatement>"
+        #@fo.puts "</letStatement>"
     end
     
     def compile_while
@@ -204,54 +222,74 @@ class CompilationEngine
     end
     
     def compile_expression
-        @fo.puts "<expression>"
+        #@fo.puts "<expression>"
         compile_term
         while apply?(TokenType::SYMBOL, ["+", "-", "*", "/", "&", "|", "<", ">", "="]) do
             if accept?(TokenType::SYMBOL, ["+", "-", "*", "/", "&", "|", "<", ">", "="])
                 compile_term
             end
         end
-        @fo.puts "</expression>"
+        #@fo.puts "</expression>"
     end
     
     def compile_term
-        @fo.puts "<term>"
-        if accept?(TokenType::INT_CONST) or accept?(TokenType::STRING_CONST) or \
-           accept?(TokenType::KEYWORD, ["true", "false", "null", "this"])
+        #@fo.puts "<term>"
+        # integerConstant | stringConstant | keywordConstant
+        if accept?(TokenType::INT_CONST) 
+            @vmwriter.write_push(:constant, @tokenizer.int_val)
+        elsif accept?(TokenType::STRING_CONST)
+
+        elsif accept?(TokenType::KEYWORD, ["true", "false", "null", "this"])
+        # [var|class|subroutine]Name
         elsif accept?(TokenType::IDENTIFIER)
+            # varName [ expression ]
             if accept?(TokenType::SYMBOL, ["["])
                 compile_expression
                 expect(TokenType::SYMBOL, ["]"])
+            # subroutineName ( expression )
             elsif accept?(TokenType::SYMBOL, ["("])
                 compile_expression_list
                 expect(TokenType::SYMBOL, [")"])
+            # (className | varName) . subroutineName ( eprexxionList )
             elsif accept?(TokenType::SYMBOL, ["."])
+                #@fo.print "call #{@tokenizer.identifier}." # className | varName
+                receiver_name = @tokenizer.identifier
                 expect(TokenType::IDENTIFIER)
+                #@fo.print "#{@tokenizer.identifier} " # function name
+                @function_name = @tokenizer.identifier
                 accept?(TokenType::SYMBOL, ["("])
                 compile_expression_list
                 expect(TokenType::SYMBOL, [")"])
+                # push aruments to the stack
+                #@fo.print "call #{receiver_name}.#{function_name} #{@expression_num}\n"
+                @vmwriter.write_call("#{receiver_name.function_name}", expression_num)
             end
+        # ( expression )
         elsif accept?(TokenType::SYMBOL, ["("])
             compile_expression
             expect(TokenType::SYMBOL, [")"])
+        # unaryOp term
         elsif accept?(TokenType::SYMBOL, ["-", "~"])
             compile_term
         end
-        @fo.puts "</term>"
+        #@fo.puts "</term>"
     end
     
     def compile_expression_list
-        @fo.puts "<expressionList>"
+        #@fo.puts "<expressionList>"
+        @expression_num = 0
         if apply?(TokenType::SYMBOL, [")"])
-            @fo.puts "</expressionList>"
+            #@fo.puts "</expressionList>"
             return
         end
         
         compile_expression
+        @expression_num += 1
         while accept?(TokenType::SYMBOL, [","]) do
             compile_expression
+            @expression_num += 1
         end
-        @fo.puts "</expressionList>"
+        #@fo.puts "</expressionList>"
     end
     
     # original
@@ -262,14 +300,14 @@ class CompilationEngine
             case @tokenizer.token_type
             when TokenType::KEYWORD
                 if token.include?(@tokenizer.keyword)
-                    @fo.puts "<keyword> #{@tokenizer.keyword} </keyword>"
+                    #@fo.puts "<keyword> #{@tokenizer.keyword} </keyword>"
                     register_key
                 else
                     return false
                 end
             when TokenType::SYMBOL
                 if token.include?(@tokenizer.symbol)
-                    @fo.puts "<symbol> #{CGI.escapeHTML(@tokenizer.symbol)} </symbol>"
+                    #@fo.puts "<symbol> #{CGI.escapeHTML(@tokenizer.symbol)} </symbol>"
                 else
                     return false
                 end
@@ -277,21 +315,19 @@ class CompilationEngine
                 #@fo.puts "<identifier> #{@tokenizer.identifier} </identifier>"
                 #p caller_locations(1).first.label
                 if @purpose == :define
-                    puts "define"
                     @symbol_table.define(@tokenizer.identifier, @type, @kind)
                     type = @type
                     kind = @kind
                 else
-                    puts "use"
-                    p type = @symbol_table.type_of(@tokenizer.identifier)
+                    type = @symbol_table.type_of(@tokenizer.identifier)
                     kind = @symbol_table.kind_of(@tokenizer.identifier)
                 end
                 index = @symbol_table.index_of(@tokenizer.identifier)
-                @fo.puts "<identifier> #{kind} #{@tokenizer.identifier} #{@purpose} #{index}</identifier>"
+                #@fo.puts "<identifier> #{kind} #{@tokenizer.identifier} #{@purpose} #{index}</identifier>"
             when TokenType::INT_CONST
-                @fo.puts "<integerConstant> #{@tokenizer.int_val} </integerConstant>"
+                #@fo.puts "<integerConstant> #{@tokenizer.int_val} </integerConstant>"
             when TokenType::STRING_CONST
-                @fo.puts "<stringConstant> #{@tokenizer.string_val} </stringConstant>"
+                #@fo.puts "<stringConstant> #{@tokenizer.string_val} </stringConstant>"
             end
             @tokenizer.advance if @tokenizer.has_more_tokens?
             true
